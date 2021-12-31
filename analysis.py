373a285a5e397eaec47e1b545424d214da5e9158
Andrew Hansraj <hansrajandrew@gmail.com>
2021-12-31 15:59:37 -0500

    Commented in needed TA tools

---
+++ analysis.py
@@ -0,0 +1,59 @@
+# Simple moving average (SMA)
+
+
+# Exponential moving average (EMA)
+
+
+# Moving average convergence divergence (MACD)
+
+
+
+# Relative strength index (RSI)
+
+
+
+# Standard deviation (SD)
+
+
+
+# Bollinger bands 
+
+
+
+# Stochastic oscillator 
+
+
+
+# On-balance volume (OBV)
+
+
+
+# Ichimoku cloud 
+
+
+
+# Fibonacci retracement level
+
+
+
+# 52-week high and 52-week low
+
+
+
+# Average volume
+
+
+
+# Price-Earning Ratio (P/E)
+
+
+
+# Bid-ask spread
+
+
+
+# Earnings per share (EPS)
+
+
+
+# Growth (1m, 3m, 1y, 5y)
